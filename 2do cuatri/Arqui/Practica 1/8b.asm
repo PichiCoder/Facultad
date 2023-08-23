cce5ac57e23f66fc4080a7ea2b0b7bf2fef24d39
Matias F <86623987+PichiCoder@users.noreply.github.com>
2023-08-23 19:02:20 -0300

    Create 8b.asm

---
+++ 2do cuatri/Arqui/Practica 1/8b.asm
@@ -0,0 +1,30 @@
+;Al no poder usar algo como CADENA DB "aBcd"00H en Vonsim, me tome la libertar de agregar un fin cadena para igualmente hacer uso del ooh que propone el ejercicio.
+
+ORG 3000H ; Subrutina Contar_Min
+Contar_Min: MOV BX, AX
+MOV DX, 0
+MOV CX, 0
+LOOP: MOV CL, [BX]
+
+CMP CL, 0
+JZ FIN ; por si nos topamos con el 00H
+
+INC BX ; tengo listo en BX la siguiente direccion
+
+SUB CL, 61H ;61H=97Dec, que es el valor de 'a' en ASCII
+JS LOOP ; si salto, el carac es MAYUSQ y paso al siguiente caracter
+
+INC DL ;NO salte, entonces cuento +1 minuscula !!!
+JMP LOOP
+FIN: ret
+
+org 1000h
+CADENA DB "aBcd"
+FIN_CADENA DB 00H
+
+org 2000h
+
+MOV AX, OFFSET CADENA
+CALL Contar_Min
+hlt
+end
