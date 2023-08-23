4096b562ef588ba5a0013400e9cac7a20a6c9477
Matias F <86623987+PichiCoder@users.noreply.github.com>
2023-08-23 19:08:52 -0300

    Update 8a.asm

--- 2do cuatri/Arqui/Practica 1/8a.asm
+++ 2do cuatri/Arqui/Practica 1/8a.asm
@@ -2,26 +2,26 @@
 ; por eso me tome la libertad de agregar la variable FIN_CADENA DB 00H
 
 ORG 3000H ; Subrutina 
-LONGITUD: MOV BX, AX
-MOV DX, 0
-MOV CX, 0
-LOOP: MOV CL, [BX]
-CMP CL, 0
-JZ FIN
-INC BX
-INC DL
-DEC CL
-JNS LOOP
+  LONGITUD: MOV BX, AX
+  MOV DX, 0
+  MOV CX, 0
+  LOOP: MOV CL, [BX]
+  CMP CL, 0
+  JZ FIN
+  INC BX
+  INC DL
+  JMP LOOP
 FIN: ret
 
 org 1000h
-CADENA DB "abcd"
-FIN_CADENA DB 00H
-RES DW ?
+
+  CADENA DB "abcd"
+  FIN_CADENA DB 00H
 
 org 2000h
 
-MOV AX, OFFSET CADENA
-CALL LONGITUD
+  MOV AX, OFFSET CADENA
+  CALL LONGITUD
+  
 hlt
 end
