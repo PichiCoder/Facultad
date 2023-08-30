da856839ea2d6aa30f466249e6cd81ee427cd2b9
Matias F <86623987+PichiCoder@users.noreply.github.com>
2023-08-30 15:54:24 -0300

    Update 10.asm

--- 2do cuatri/Arqui/Practica 1/10.asm
+++ 2do cuatri/Arqui/Practica 1/10.asm
@@ -1,5 +1,26 @@
 ORG 3000h
-SWAP:
+SWAP: MOV BX, SP
+ADD BX, 2 ; me paro en la direccion guardada de segundo
+MOV CX, [BX] ; queda en CX direccion de segundo
+
+ADD BX, 2 ; me paro en la direccion guardada de primero en la pila
+MOV AX, [BX] ; queda en AX direccion de primero
+
+MOV BX, CX
+MOV CX, [BX] ; tengo segundo en CX
+
+MOV BX, AX ; pongo la direccion de primero en BX
+MOV DX, [BX] ; me guardo primero en DX
+
+MOV WORD PTR [BX], CX ; pongo a segundo en la direccion de primero
+
+MOV BX, SP
+ADD BX, 2 ; me paro en la direccion guardada de segundo
+MOV CX, [BX] ; queda en CX direccion de segundo
+MOV BX, CX ; BX apunta a segundo
+
+MOV WORD PTR [BX], DX ; pongo a primero en la direccion de segundo
+
 ret
 
 ORG 1000h
@@ -7,7 +28,7 @@ primero DW 1111H
 segundo DW 2222H
 
 ORG 2000h
-mov AX, offset primero
+MOV AX, offset primero
 MOV CX, offset segundo
 PUSH AX
 PUSH CX
