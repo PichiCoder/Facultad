3a21c2932f591d629571c576a1e8cb8479dfce0c
Matias F <86623987+PichiCoder@users.noreply.github.com>
2023-09-03 13:38:50 -0300

    Create 5.asm

---
+++ 2do cuatri/Arqui/Practica 2/5.asm
@@ -0,0 +1,32 @@
+;Modificar el programa anterior agregando una subrutina llamada ES_NUM que verifique si el caracter ingresado es
+; realmente un número. De no serlo, el programa debe mostrar el mensaje “CARACTER NO VALIDO”. La subrutina debe
+; recibir el código del caracter por referencia desde el programa principal y debe devolver vía registro el valor 0FFH en caso
+; de tratarse de un número o el valor 00H en caso contrario. 
+;Tener en cuenta que el código del “0” es 30H y el del “9” es 39H.
+
+ORG 1000H
+MSJ DB "INGRESE UN NUMERO:"
+FIN DB ?
+MSJ2 DB "CARACTER NO VALIDO"
+FIN2 DB ?
+
+ORG 1500H
+NUM DB ?
+
+ORG 3000h
+ES_NUM: 
+ret
+
+ORG 2000H
+
+MOV BX, OFFSET MSJ
+MOV AL, OFFSET FIN-OFFSET MSJ
+INT 7
+MOV BX, OFFSET NUM
+INT 6
+MOV AL, 1
+INT 7
+MOV CL, NUM
+
+INT 0
+END
