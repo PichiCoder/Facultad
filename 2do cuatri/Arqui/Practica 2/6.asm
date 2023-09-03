e4386039e6f55311df9c58871f3b2ff0bc22fac3
Matias F <86623987+PichiCoder@users.noreply.github.com>
2023-09-03 20:28:00 -0300

    Update 6.asm

--- 2do cuatri/Arqui/Practica 2/6.asm
+++ 2do cuatri/Arqui/Practica 2/6.asm
@@ -1,4 +1,4 @@
-;6 - Escribir un programa que solicite el ingreso de un número (de un dígito) por teclado y muestre en pantalla dicho
+;Escribir un programa que solicite el ingreso de un número (de un dígito) por teclado y muestre en pantalla dicho
 ; número expresado en letras. Luego que solicite el ingreso de otro y así sucesivamente. Se debe finalizar la ejecución al
 ; ingresarse en dos vueltas consecutivas el número cero.
 
@@ -6,30 +6,44 @@ ORG 1000H
 
 MSJ DB "INGRESE UN NUMERO:"
 FIN1 DB ?
-MSJ2 DB "CARACTER NO VALIDO"
+tabla_nros DB 1,2,3,4,5,6,7,8,9
+nros DB "cero", "uno", "dos", "tres", "cuatro", "cinco", "seis", "siete", "ocho", "nueve"
+enLetras DB ?
 FIN2 DB ?
 
 ORG 1500H
 
-NUM DB ?
+NUM DB ? ; recordar que se guarda en codigo ascii (30h a 39h)
 
 ORG 3000h
-ret 
-
-ORG 2000H
-
-MOV BX, OFFSET MSJ
+inicio: MOV BX, OFFSET MSJ
 MOV AL, OFFSET FIN1-OFFSET MSJ
 INT 7
+ret 
 
-MOV BX, OFFSET NUM
+input: MOV BX, OFFSET NUM
 INT 6
+ret
 
+output:
+ret
 
-es_Char: MOV BX, OFFSET NUM
-MOV AL, 1
-INT 7
-MOV CL, NUM
+contar0s: INC AH
+ret
 
-INT 0
+ORG 2000H
+MOV AH, 00h ; para contar ceros despues
+
+loop: CALL inicio ;leer msj incial
+CALL input ;ingresar digito
+
+CMP NUM, 00h ;para ver si es un cero
+JZ contar0s
+CMP AH, 2 ; si da 0 es porque el 0 ya se ingreso dos veces y hay que terminar.
+JZ final 
+
+CALL output ;imprimir el nro en palabras. Ej 7 --> 'siete'
+JMP loop
+
+final: INT 0
 END
