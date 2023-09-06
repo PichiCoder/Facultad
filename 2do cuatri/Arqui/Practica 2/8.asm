8d94725939bf5dc2f976b5b5247f6dd913e291ff
Matias F <86623987+PichiCoder@users.noreply.github.com>
2023-09-06 15:13:04 -0300

    Update 8.asm

--- 2do cuatri/Arqui/Practica 2/8.asm
+++ 2do cuatri/Arqui/Practica 2/8.asm
@@ -1,1 +1,3 @@
-asd
+: Escribir un programa que efectúe la resta de dos números (de un dígito cada uno) ingresados por teclado y muestre el
+resultado en la pantalla de comandos. Antes de visualizarlo el programa debe verificar si el resultado es positivo o negativo
+y anteponer al valor el signo correspondiente.
