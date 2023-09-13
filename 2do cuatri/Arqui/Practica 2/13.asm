8424f7b2e444c0192fd1ae4da75779dff58c0e6b
Matias F <86623987+PichiCoder@users.noreply.github.com>
2023-09-13 18:55:11 -0300

    Update 13.asm

--- 2do cuatri/Arqui/Practica 2/13.asm
+++ 2do cuatri/Arqui/Practica 2/13.asm
@@ -1,1 +1,65 @@
-; Modificar el programa anterior para que también cuente minutos (00:00 - 59:59), pero que actualice la visualización en pantalla cada 10 segundos
+;13) Modificar el programa anterior para que también cuente minutos (00:00 - 59:59), 
+;pero que actualice la visualización en pantalla cada 10 segundos
+
+CONT EQU 10H
+COMP EQU 11H
+EOI EQU 20H
+IMR EQU 21H
+TIMER EQU 25H
+
+POS_VECTOR_CLK EQU 10
+
+ORG 40
+DIR_RUT_CLK DW RUTINA_CLK
+
+ORG 1000H
+SEG DB 30H
+DB 30H
+FINS DB ?
+
+MIN DB 30H
+DB 30H
+FINM DB ?
+
+ORG 3000H
+RUTINA_CLK: PUSH AX
+INC SEG+1
+CMP SEG+1, 3AH
+JNZ RESET
+MOV SEG+1, 30H
+INC SEG
+CMP SEG, 36H
+JNZ RESET
+MOV SEG, 30H
+RESET: INT 7
+MOV AL, 0
+OUT CONT, AL ; Aca seteo el conteo a 0
+MOV AL, EOI
+OUT EOI, AL
+POP AX
+IRET
+
+ORG 2000H
+CLI
+;config del PIC
+MOV AL, 0FDH ; muevo 1111 1101 AL IMR para tener habilitado el Timer
+OUT IMR, AL ; recordar que OUT es escribir en memoria de E/S
+
+MOV AL, POS_VECTOR_CLK
+OUT TIMER, AL ; pongo ID en INT1
+;
+
+;config del Timer
+MOV AL, 10
+OUT COMP, AL ; COMP lo pongo en 10 porque quiero la interrupcion cada 10 segundos
+MOV AL, 0
+OUT CONT, AL
+;
+MOV BX, OFFSET SEG
+MOV AL, OFFSET FINS-OFFSET SEG
+
+STI
+
+LAZO: JMP LAZO
+
+END
