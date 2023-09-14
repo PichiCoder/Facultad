bdd948d691fcbed7750c3d8a51f44efd1824b958
Matias F <86623987+PichiCoder@users.noreply.github.com>
2023-09-14 14:14:05 -0300

    Update 14.asm

--- 2do cuatri/Arqui/Practica 2/14.asm
+++ 2do cuatri/Arqui/Practica 2/14.asm
@@ -17,56 +17,62 @@ ORG 80
 DIR_RUTINA_F10 DW RUTINA_F10
 
 ORG 1000H
-flag DB 1
+SEG DB 30H ; Decena
+DB 30H ; Unidad
+FIN DB ?
 
 ORG 3000H
-RUTINA_F10: PUSH AX
-;Accion del F10: quiero que al presionarlo se reinicie el timer!
-;Entonces hago lo siguiente:
-MOV AL, 0
-OUT CONT, AL ; seteo el conteo a 0 para reiniciar el Timer
-;
-MOV AL, EOI
-OUT EOI, AL ; ESCRIBO 20h en para indicar que atendi el F10
-POP AX
-IRET
 
-RUTINA_CLK: PUSH AX
-; solo me tengo que preocupar de que si el conteo llego a 30 y no se presiono el F10 se tiene que resetear el conteo.
-MOV AL, 0
-OUT CONT, AL ; seteo el conteo a 0 para reiniciar el Timer
-; Como variante, si quisiera que el programa termine al llegar a contar los 30 segundos haria lo siguiente con mi variable flag:
-;" MOV flag, 0 ", y en el programa ppal pongo el loop aprovechando esta condicion para cortar.
-MOV AL, EOI
+RUTINA_CLK: PUSH AX ; Se guarda el valor de AX, porque se va a usar el registro
+INC SEG+1
+CMP SEG+1, 3AH
+JNZ RESET
+MOV SEG+1, 30H
+INC SEG
+CMP SEG, 33H
+JNZ RESET
+MOV DL, 1 ; Pongo en TRUE el flag de finalizacion
+MOV AL, 0FFH ; Deshabilito interrupciones en IMR
+OUT IMR, AL
+RESET: MOV AL, 2 ; El contador tiene 2 caracteres
+INT 7 ; Se imprime el valor actual
+MOV AL, 0 ; Se vuelve a cero el contador del TIMER
+OUT CONT, AL
+MOV AL, EOI ; Se finaliza la atencion de la interrupcion
 OUT EOI, AL
-POP AX
+POP AX ; Se recupera el valor que contenia AX al entrar en la rutina
 IRET
 
+ORG 3500H
+
+RUTINA_F10: PUSH AX ; Se guarda el valor de AX, porque se va a usar el registro
+IN AL, IMR ; Recupero el valor actual del IMR
+XOR AL, 00000010B ; Y cambio la linea correspondiente al TIMER
+OUT IMR, AL
+MOV AL, EOI ; Se finaliza la atencion de la interrupcion
+OUT EOI, AL
+POP AX ; Se recupera el valor que contenia AX al entrar en la rutina
+IRET
 ORG 2000H
 CLI
-;config del PIC
-MOV AL, 0FCH ; muevo 1111 1100 AL IMR para tener habilitado el Timer y el F10
-OUT IMR, AL ; recordar que OUT es escribir en memoria de E/S
-
+MOV AL, 0FEH
+OUT IMR, AL ; PIC: registro IMR
 MOV AL, POS_VECTOR_F10
-OUT INT0, AL ; pongo ID en INT0
+OUT INT0, AL ; PIC: registro INT0, F10
 
 MOV AL, POS_VECTOR_CLK
-OUT INT1, AL ; pongo ID en INT1
-;
+OUT INT1, AL ; PIC: registro INT1, TIMER
 
-;config del Timer
-MOV AL, 30
-OUT COMP, AL ; COMP lo pongo en 30 porque quiero la interrupcion cada 30 segundos
+MOV AL, 1
+OUT COMP, AL ; TIMER: registro COMP
 MOV AL, 0
-OUT CONT, AL
+OUT CONT, AL ; TIMER: registro CONT
+MOV BX, OFFSET SEG ; Direccion del contador
+MOV DL, 0
 STI
 
-loop: JMP loop
-
-; El loop si usara la variante para cortar el programa:
-;" loop: CMP flag, 0 "
-;" JNZ loop "
+LAZO: CMP DL, 0
+JZ LAZO
 
 INT 0
 END
