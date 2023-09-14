b989061689468c9e0119906c003c1e78337dff53
Matias F <86623987+PichiCoder@users.noreply.github.com>
2023-09-14 14:31:25 -0300

    Update 14.asm

--- 2do cuatri/Arqui/Practica 2/14.asm
+++ 2do cuatri/Arqui/Practica 2/14.asm
@@ -16,14 +16,21 @@ DIR_RUT_CLK DW RUTINA_CLK
 ORG 80
 DIR_RUTINA_F10 DW RUTINA_F10
 
-ORG 1000H
-SEG DB 30H ; Decena
-DB 30H ; Unidad
-FIN DB ?
-
 ORG 3000H
 
-RUTINA_CLK: PUSH AX ; Se guarda el valor de AX, porque se va a usar el registro
+RUTINA_F10: PUSH AX ; Se guarda el valor de AX porque se va a usar
+IN AL, IMR ; Recupero el valor actual del IMR
+XOR AL, 00000010B ; Y cambio la linea correspondiente al TIMER 
+OUT IMR, AL ; aca habilito el uso del Timer
+MOV AL, EOI ; Se finaliza la atencion de la interrupcion F10
+OUT EOI, AL
+POP AX ; Se recupera el valor que contenia AX al entrar en la rutina
+IRET
+
+ORG 3500H
+
+RUTINA_CLK: PUSH AX ; Se guarda el valor de AX, porque se va a usar
+;Lo que tiene que ver con el incremento de SEG y SEG+1 es lo mismo que lo del punto 12 para imprimir el paso de los segundos.
 INC SEG+1
 CMP SEG+1, 3AH
 JNZ RESET
@@ -31,6 +38,7 @@ MOV SEG+1, 30H
 INC SEG
 CMP SEG, 33H
 JNZ RESET
+;
 MOV DL, 1 ; Pongo en TRUE el flag de finalizacion
 MOV AL, 0FFH ; Deshabilito interrupciones en IMR
 OUT IMR, AL
@@ -43,33 +51,28 @@ OUT EOI, AL
 POP AX ; Se recupera el valor que contenia AX al entrar en la rutina
 IRET
 
-ORG 3500H
+ORG 1000H
 
-RUTINA_F10: PUSH AX ; Se guarda el valor de AX, porque se va a usar el registro
-IN AL, IMR ; Recupero el valor actual del IMR
-XOR AL, 00000010B ; Y cambio la linea correspondiente al TIMER
-OUT IMR, AL
-MOV AL, EOI ; Se finaliza la atencion de la interrupcion
-OUT EOI, AL
-POP AX ; Se recupera el valor que contenia AX al entrar en la rutina
-IRET
+SEG DB 30H ; Decena
+DB 30H ; Unidad
+FIN DB ?
 
 ORG 2000H
 
 CLI
 MOV AL, 0FEH
-OUT IMR, AL ; PIC: registro IMR
+OUT IMR, AL ; se configura IMR para que solo ande el F10
 MOV AL, POS_VECTOR_F10
-OUT INT0, AL ; PIC: registro INT0, F10
+OUT INT0, AL ; config de INT0
 
 MOV AL, POS_VECTOR_CLK
-OUT INT1, AL ; PIC: registro INT1, TIMER
+OUT INT1, AL ; config del timer
 
 MOV AL, 1
-OUT COMP, AL ; TIMER: registro COMP
+OUT COMP, AL ; pongo 1 en COMP
 MOV AL, 0
-OUT CONT, AL ; TIMER: registro CONT
-MOV BX, OFFSET SEG ; Direccion del contador
+OUT CONT, AL ; pongo 0 en CONT
+MOV BX, OFFSET SEG ; me guardo Direccion del contador
 MOV DL, 0
 STI
 
