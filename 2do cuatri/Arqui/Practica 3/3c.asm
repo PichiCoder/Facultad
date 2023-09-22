4b65f77520a514042623316e29cf2a2c52a84dfe
Matias F <86623987+PichiCoder@users.noreply.github.com>
2023-09-22 19:21:08 -0300

    Update 3c.asm

--- 2do cuatri/Arqui/Practica 3/3c.asm
+++ 2do cuatri/Arqui/Practica 3/3c.asm
@@ -1,3 +1,7 @@
+; c) Escribir un programa que imprime “UNIVERSIDAD NACIONAL DE LA PLATA” en la impresora a
+; través del HAND-SHAKE. La comunicación se establece por interrupciones emitidas desde el
+; HAND-SHAKE cada vez que la impresora se desocupa.
+
 ;PIC
 EOI EQU 20H
 IMR EQU 21H
@@ -5,28 +9,28 @@ INT0 EQU 24H ;para F10
 INT1 EQU 25H ;para el Timer
 INT2 EQU 26H ;para impresora
 ;hasta INT7 en 2BH
-
-POS_VECTOR_IMPRESORA EQU 20
-
 ;HANDSHAKE
 DATO EQU 40H
 ESTADO EQU 41H
 
-ORG 80H
+POS_VECTOR_IMPRESORA EQU 20
+
+ORG 80
 DIR_RUT_IMPRESORA DW RUT_IMPRESORA
 
 ORG 3000H
-RUT_IMPRESORA: PUSH AX
-;
-MOV AL, [BX]
-OUT DATO, AL
-INC BX
-DEC CL
-;
-MOV AL, 20H
-OUT EOI, AL
-POP AX
- IRET
+RUT_IMPRESORA:MOV AL, [BX]
+              OUT DATO, AL
+              INC BX
+              DEC CL
+              JNZ volver
+              ;Desactivar interrupciones si ya terminamos de imprimir
+              MOV AL, 0FFH
+              OUT IMR, AL
+              
+              volver: MOV AL, 20H
+              OUT EOI, AL
+              IRET
 
 ORG 1000H
 str DB "UNIVERSIDAD NACIONAL DE LA PLATA"
@@ -35,22 +39,22 @@ fin db ?
 ORG 2000H
 CLI
 ;CONFIG DEL PIC
-MOV AL, 11111011b ; 1111 1011 al IMR 
+MOV AL, 11111011b ; 1111 1011 al IMR para habilitar impresora
 OUT IMR, AL
 MOV AL, POS_VECTOR_IMPRESORA
 OUT INT2, AL
+
+; Config Handshake para interrupcion
+IN AL, ESTADO ; Tomo estado actual
+OR AL, 80H ; 80H = 10000000
+OUT ESTADO, AL ; Estado = 1xxxxxxx
 
 MOV BX, OFFSET str
 MOV CL, OFFSET fin - OFFSET str
 STI
 
-; Configuro el Handshake para interrupcion
-IN AL, ESTADO ; Tomo estado actual
-OR AL, 80H ; 80H = 10000000
-OUT ESTADO, AL ; Estado = 1xxxxxxx
-
 loop: CMP CL, 0
-JNZ loop
+JNZ loop
 
-INT 0
+INT 0
 END
